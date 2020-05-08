99fd89931dc3bac1285003e3de020e07177ba4f8
Winderps <homerun32295@gmail.com>
2020-05-08 12:20:44 -0400

    Changed project type

--- POSTerminalGui/Program.cs
+++ POSTerminalGui/Program.cs
@@ -2,23 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace POSTerminalGui
 {
     static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            Console.WriteLine("Coding is fun!");
         }
-        //this is a test
     }
 }
