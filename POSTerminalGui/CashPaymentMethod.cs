89bcdeaa16fd7dc7490b1c9fd80dec00871c671a
chris <caconstan@yahoo.com>
2020-05-08 13:56:10 -0400

    first commit

---
+++ POSTerminalGui/CashPaymentMethod.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POSTerminalGui
+{
+    class CashPaymentMethod
+    {
+        public bool TakePayment(double amountDue, out double amountStillDue)
+        {
+           //For cash, ask the amount tendered and provide change.
+           //For check, get the check number.
+           //For credit, get the credit card number, expiration, and CW.
+
+           Console.WriteLine("Please entered the amount tendered: ");
+           double amountTendered = 0;
+           while (!double.TryParse(Console.ReadLine(), out amountTendered) ||
+                amountTendered <= 0)
+            {
+                Console.WriteLine("I'm sorry I didn't understand.  Please try again.");
+            }
+
+           if (amountTendered >= amountDue)
+            {
+                double change = amountTendered - amountDue;
+                Console.WriteLine("Here is your change:" + change);
+                amountStillDue = 0;
+                return true;
+            } else
+            {
+                amountStillDue = amountDue - amountTendered;
+                Console.WriteLine("Amount still due:" + amountStillDue);
+                return false;
+            }
+        }
+    }
+}
---
+++ POSTerminalGui/CheckPaymentMethod.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POSTerminalGui
+{
+    class CheckPaymentMethod
+    {
+        public bool TakePayment(double amountDue, out double amountStillDue)
+        {
+            //For check, get the check number.
+
+            Console.WriteLine("Please entered the check number: ");
+            int checkNumber = 0;
+            while (int.TryParse(Console.ReadLine(), out checkNumber) ||
+                 checkNumber <= 0)
+            {
+                Console.WriteLine("I'm sorry I didn't understand.  Please try again.");
+            }
+
+            Console.WriteLine("Please entered the amount tendered: ");
+            double amountTendered = 0;
+            while (double.TryParse(Console.ReadLine(), out amountTendered) ||
+                 amountTendered <= 0)
+            {
+                Console.WriteLine("I'm sorry I didn't understand.  Please try again.");
+            }
+
+            amountStillDue = amountDue - amountTendered;
+
+            if (amountStillDue < 0)
+            {
+                Console.WriteLine("Check cannot be for more than the amount due");
+                amountStillDue = amountDue;
+                return false;
+            }
+            else if (amountStillDue == 0) {
+                return true;
+            }
+            else // amountStillDue > 0
+            {                
+                Console.WriteLine("Amount still due:" + amountStillDue);
+                return false;
+            }
+
+        }
+    }
+}
