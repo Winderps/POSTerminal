b9f40a713870779123d9355a5c175dcdd1e27a97
Winderps <homerun32295@gmail.com>
2020-05-08 18:57:26 -0400

    Base functionality for Order and OrderView

---
+++ POSTerminalGui/Order.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace POSTerminalGui
+{
+    class Order
+    {
+        private const double SalesTax = .06;
+
+        public Dictionary<Product, int> Contents { get; set; }
+        public List<PaymentMethod> PaymentMethods { get; set; }
+        public bool Paid { get; set; }
+
+        public Order()
+        {
+            Contents = new Dictionary<Product, int>();
+            PaymentMethods = new List<PaymentMethod>();
+            Paid = false;
+        }
+
+        public void AddProduct(Product product, int quantity)
+        {
+            if (quantity <= 0)
+            {
+                throw new ArgumentException("Amount must be greater than 0!");
+            }
+            Contents.Add(product, quantity);
+        }
+
+        public double GetSubtotal()
+        {
+            double total = 0.0;
+            foreach(KeyValuePair<Product, int> item in Contents)
+            {
+                total += (item.Key.Price * (double)item.Value);
+            }
+            return total;
+        }
+
+        public double GetSalesTax()
+        {
+            double taxTotal = 0.0;
+            foreach (KeyValuePair<Product, int> item in Contents)
+            { 
+                // if (Product.Taxable)
+                taxTotal += item.Key.Price * SalesTax;
+            }
+            return taxTotal;
+        }
+
+        public double GetGrandTotal()
+        {
+            return GetSubtotal() + GetSalesTax();
+        }
+
+        public static double GetLineSubtotal(KeyValuePair<Product, int> orderItem)
+        {
+            return (orderItem.Key.Price * orderItem.Value);
+        }
+
+        public static double GetLineTotal(KeyValuePair<Product, int> orderItem)
+        {
+            return (orderItem.Key.Price * orderItem.Value) * (1.0 + SalesTax);
+        }
+
+        public static double GetTaxAmount(KeyValuePair<Product, int> orderItem)
+        {
+            //if (!orderItem.Key.Taxable) return 0.0;
+            return (orderItem.Key.Price * orderItem.Value) * SalesTax;
+        }
+    }
+}
---
+++ POSTerminalGui/OrderView.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POSTerminalGui
+{
+    class OrderView
+    {
+        public Order Transaction { get; set; }
+
+        public OrderView(Order transaction)
+        {
+            Transaction = transaction;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{"Item Name",20}{"Department",15}{"Amt@Price",15}{"Tax",10}{"Subtotal",15}{"Total",15}");
+            Console.WriteLine(new String('=', 90));
+
+            foreach (KeyValuePair<Product, int> item in Transaction.Contents)
+            {
+                Product p = item.Key;
+                double lineSubtotal = Order.GetLineSubtotal(item);
+                double tax = Order.GetTaxAmount(item);
+                Console.WriteLine(
+                    $"{p.Name,20}" +
+                    $"{p.ProductCategory,15}" +
+                    $"{item.Value+"@"+p.Price.ToString("C2"),15}" +
+                    $"{Order.GetTaxAmount(item),10:C2}" +
+                    $"{Order.GetLineSubtotal(item),15:C2}" +
+                    $"{lineSubtotal + tax,15:C2}");
+            }
+
+            Console.WriteLine(new String('=',90));
+            Console.WriteLine(
+                $"{"Totals",6}" +
+                $"{Transaction.GetSalesTax(),54:C2}" +
+                $"{Transaction.GetSubtotal(),15:C2}" +
+                $"{Transaction.GetGrandTotal(),15:C2}"
+                );
+        }
+
+        public static void TestMe()
+        {
+            List<Product> p = Product.GetProducts();
+
+            Order o = new Order();
+            OrderView view = new OrderView(o);
+            o.AddProduct(p[0], 3);
+            o.AddProduct(p[1], 1);
+            o.AddProduct(p[4], 5);
+            view.Display();
+        }
+    }
+}
